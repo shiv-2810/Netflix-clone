e0081e39b168beb29d324e239d75194a857fa363
shiv-2810 <s9431494772@gmail.com>
2022-07-18 13:13:57 +0530

    Project setup done

---
+++ netflix/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCiPK4WxvIgARzhQQC9939QpBjuBK91xig",
+    authDomain: "netflix-144db.firebaseapp.com",
+    projectId: "netflix-144db",
+    storageBucket: "netflix-144db.appspot.com",
+    messagingSenderId: "1097010370677",
+    appId: "1:1097010370677:web:37327a5219eef8110d803c"
+  };
